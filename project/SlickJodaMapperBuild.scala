e7cf2d1b646dc8e1aae23f83fbdcbec4a57dd286
toshiyuki <t.toshi.0412@gmail.com>
2013-04-13 22:36:28 +0900

    Bumped version

--- project/SlickJodaMapperBuild.scala
+++ project/SlickJodaMapperBuild.scala
@@ -9,7 +9,7 @@ object SlickJodaMapperBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "slick-joda-mapper",
       organization := "com.github.tototoshi",
-      version := "0.2.0",
+      version := "0.2.1-SNAPSHOT",
       scalaVersion := "2.10.0",
       scalacOptions ++= Seq("-deprecation", "-language:_"),
       libraryDependencies ++= Seq(
